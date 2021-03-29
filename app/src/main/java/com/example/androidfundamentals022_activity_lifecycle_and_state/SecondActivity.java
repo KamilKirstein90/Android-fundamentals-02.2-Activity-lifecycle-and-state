6c425b26edaaebf2c316b75dee2248af11b2430b
Kamil Kirstein <Kamilkirstein@googlemail.com>
2021-03-29 19:54:49 +0200

    [AF022] Created the view in Main Act
    Elefen text views and one btn in Main Act.

---
+++ app/src/main/java/com/example/androidfundamentals022_activity_lifecycle_and_state/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidfundamentals022_activity_lifecycle_and_state;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
