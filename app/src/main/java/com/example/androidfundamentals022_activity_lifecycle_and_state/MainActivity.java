90fdd017ec9bd5f9f4f39eeb8a1d3ab06ffce73a
Kamil Kirstein <Kamilkirstein@googlemail.com>
2021-03-31 07:37:06 +0200

    [AF022] No ActionBar onActivityResult
    In AndroidMainfest.xml the theme is changed to @style/Theme.Design.NoActionBar to fit all new elements inside the main and second activity in the app.  (I removed the action bar)
    
    In MainActivity addItemFromSecondAct is created where we start the second Activity with the right req_code for resulst and onActivityResult is implemented to get the right result.
    
    In SecondActivity an onClick method is called and the text of every clicked button is put to an extra of an intent and the intent ist then set as Result and the activity is finished.

--- app/src/main/java/com/example/androidfundamentals022_activity_lifecycle_and_state/MainActivity.java
+++ app/src/main/java/com/example/androidfundamentals022_activity_lifecycle_and_state/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.androidfundamentals022_activity_lifecycle_and_state;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    final static int  REQ_SECOND_ACT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // in this overridden function we get the results from intents that we started with startActivityForResult
+    // the request code is the key for the activity we started and want to get results from
+    // the result code ist to check if the intent has results was successful or canceled
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // check if result code  was ok
+        // check for the right request code
+        // check if data is not null
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // to save the items when the phones rotation is changed
+    }
+
+    public void addItemFromSecondAct(View view) {
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, REQ_SECOND_ACT);
     }
 }
--- app/src/main/java/com/example/androidfundamentals022_activity_lifecycle_and_state/SecondActivity.java
+++ app/src/main/java/com/example/androidfundamentals022_activity_lifecycle_and_state/SecondActivity.java
@@ -2,13 +2,31 @@ package com.example.androidfundamentals022_activity_lifecycle_and_state;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    final static String EXTRA_ITEM_VALUE = "SecondActivity.extra.itemValue";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+    // in this method a returnIntent with the key for the item value of this act is passed with the right button text
+    // the intent is set as result and the activity finished
+    // in the main act the result will be fetched  in
+
+    public void addItemToMainAct(View view) {
+        Intent returnIntent = new Intent();
+        // to get the text from the button, a real button instance has to be created and initialized with a cast of the view type
+        // then the button methods like getText() can be used
+        Button b = (Button) view;
+        returnIntent.putExtra(EXTRA_ITEM_VALUE, b.getText().toString());
+        setResult(RESULT_OK, returnIntent);
+        finish();
     }
 }
